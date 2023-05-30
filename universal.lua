e12d395477f1f22265845e839305a28af33a4236
Useless <91428127+Dimanoname@users.noreply.github.com>
2023-05-30 10:21:19 +0300

    Delete universal.lua

--- universal.lua
+++
@@ -1,4 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/1/main/Anti-AFK.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/1/main/ServerhopGUI"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/1/main/FPScount"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/1/main/rejoinGui"))()
