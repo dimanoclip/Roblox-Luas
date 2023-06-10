1dbe04eb1407ae5db8d3ebd23475bf8615f47e17
Useless <91428127+Dimanoname@users.noreply.github.com>
2023-06-10 15:23:47 +0300

    1

--- messages.lua
+++ messages.lua
@@ -3,56 +3,56 @@ Library.ChatMessage = function(text, color)
     if color:lower() == "red" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
             Text = text;
-            Font = Enum.Font.Cartoon;
+            Font = Enum.Font.SciFi;
             Color = Color3.fromRGB(190,0,0);
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "green" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
             Text = text;
-            Font = Enum.Font.Cartoon;
+            Font = Enum.Font.SciFi;
             Color = Color3.fromRGB(0,255,190);
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "blue" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
             Text = text;
-            Font = Enum.Font.Cartoon;
+            Font = Enum.Font.SciFi;
             Color = Color3.fromRGB(0,120,255);
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "purple" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
             Text = text;
-            Font = Enum.Font.Cartoon;
+            Font = Enum.Font.SciFi;
             Color = Color3.fromRGB(200,0,255);
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "gold" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
             Text = text;
-            Font = Enum.Font.Cartoon;
+            Font = Enum.Font.SciFi;
             Color = Color3.fromRGB(255, 200,0);
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "orange" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
             Text = text;
-            Font = Enum.Font.Cartoon;
+            Font = Enum.Font.SciFi;
             Color = Color3.fromRGB(255,150,0);
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "black" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
             Text = text;
-            Font = Enum.Font.Cartoon;
+            Font = Enum.Font.SciFi;
             Color = Color3.fromRGB(0,0,0);
             FontSize = Enum.FontSize.Size96
         })
     else
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
             Text = text;
-            Font = Enum.Font.Cartoon;
+            Font = Enum.Font.SciFi;
             Color = Color3.fromRGB(255,255,255);
             FontSize = Enum.FontSize.Size96
         })
