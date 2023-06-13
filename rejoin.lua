f15866b9b5517733e054165944d0458159bd90a0
Useless <91428127+Dimanoname@users.noreply.github.com>
2023-06-13 23:44:08 +0300

    1

--- rejoin.lua
+++ rejoin.lua
@@ -2,14 +2,11 @@ local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
  
-local Rejoin = coroutine.create(function()
-    local Success, ErrorMessage = pcall(function()
-        TeleportService:Teleport(game.PlaceId, LocalPlayer)
-    end)
- 
-    if ErrorMessage and not Success then
-        warn(ErrorMessage)
-    end
+local Success, ErrorMessage = pcall(function()
+    TeleportService:Teleport(game.PlaceId, LocalPlayer)
 end)
- 
-coroutine.resume(Rejoin)
+
+if ErrorMessage and not Success then
+    warn(ErrorMessage)
+end
+end)
