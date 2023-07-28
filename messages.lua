12c664ad463b48ac8635a90577678891960c9f91
Useless <91428127+Dimanoname@users.noreply.github.com>
2023-07-28 21:54:51 +0300

    1

--- messages.lua
+++ messages.lua
@@ -1,66 +1,67 @@
 local Library = {}
 Library.Chat = function(text, color)
+    if not game.StarterGui:GetCore("ChatActive") then game.StarterGui:SetCore("ChatActive", true) end
     if color:lower() == "red" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
-            Text = tostring(text);
-            Font = Enum.Font.Code;
-            Color = Color3.fromRGB(190,0,0);
+            Text = text,
+            Font = Enum.Font.Code,
+            Color = Color3.fromRGB(190,0,0),
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "green" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
-            Text = tostring(text);
-            Font = Enum.Font.Code;
-            Color = Color3.fromRGB(0,255,190);
+            Text = text,
+            Font = Enum.Font.Code,
+            Color = Color3.fromRGB(0,255,190),
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "blue" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
-            Text = tostring(text);
-            Font = Enum.Font.Code;
-            Color = Color3.fromRGB(0,120,255);
+            Text = text,
+            Font = Enum.Font.Code,
+            Color = Color3.fromRGB(0,120,255),
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "purple" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
-            Text = tostring(text);
-            Font = Enum.Font.Code;
-            Color = Color3.fromRGB(200,0,255);
+            Text = text,
+            Font = Enum.Font.Code,
+            Color = Color3.fromRGB(200,0,255),
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "gold" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
-            Text = tostring(text);
-            Font = Enum.Font.Code;
-            Color = Color3.fromRGB(255, 200,0);
+            Text = text,
+            Font = Enum.Font.Code,
+            Color = Color3.fromRGB(255, 200,0),
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "orange" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
-            Text = tostring(text);
-            Font = Enum.Font.Code;
-            Color = Color3.fromRGB(255,150,0);
+            Text = text,
+            Font = Enum.Font.Code,
+            Color = Color3.fromRGB(255,150,0),
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "black" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
-            Text = tostring(text);
-            Font = Enum.Font.Code;
-            Color = Color3.fromRGB(0,0,0);
+            Text = text,
+            Font = Enum.Font.Code,
+            Color = Color3.fromRGB(0,0,0),
             FontSize = Enum.FontSize.Size96
         })
     elseif color:lower() == "gray" then
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
-            Text = tostring(text);
-            Font = Enum.Font.Code;
-            Color = Color3.fromRGB(6,60,60);
+            Text = text,
+            Font = Enum.Font.Code,
+            Color = Color3.fromRGB(6,60,60),
             FontSize = Enum.FontSize.Size96
         })
     else
         game.StarterGui:SetCore("ChatMakeSystemMessage", {
-            Text = tostring(text);
-            Font = Enum.Font.Code;
-            Color = Color3.fromRGB(255,255,255);
+            Text = text,
+            Font = Enum.Font.Code,
+            Color = Color3.fromRGB(255,255,255),
             FontSize = Enum.FontSize.Size96
         })
     end
@@ -70,19 +71,19 @@ Library.Notify = function(title, text, dur)
     if dur == nil then
         game.StarterGui:SetCore("SendNotification", {
             Title = tostring(title),
-            Text = tostring(text),
+            Text = text,
             Duration = 1
         })
     elseif dur < 0 then
         game.StarterGui:SetCore("SendNotification", {
             Title = tostring(title),
-            Text = tostring(text),
+            Text = text,
             Duration = math.huge
         })
     else
         game.StarterGui:SetCore("SendNotification", {
             Title = tostring(title),
-            Text = tostring(text),
+            Text = text,
             Duration = dur
         })
     end
