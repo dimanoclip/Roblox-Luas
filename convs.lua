35ad1e6b154370e215701fa0aff708b3d03fb8f1
Useless <91428127+Dimanoname@users.noreply.github.com>
2023-08-03 03:01:22 +0300

    1

--- convs.lua
+++ convs.lua
@@ -1,73 +1,70 @@
 local Library = {}
-
 Library.Round = function(num, numDecimalPlaces)
 	local mult = 10^(numDecimalPlaces or 0)
 	return math.floor(num * mult + 0.5) / mult
 end
-
-Library.ToLetters = function(num)
+Library.ToLetters = function(num, commanums)
 	if num / 1e57 >=1 then
 		newnum = num / 1e303
-		return Library.Round(newnum, 2).. "Ce"
+		return Library.Round(newnum, (commanums or 2)).. "Ce"
 	elseif num / 1e57 >=1 then
 		newnum = num / 1e57
-		return Library.Round(newnum, 2).. "OD"
+		return Library.Round(newnum, (commanums or 2)).. "OD"
 	elseif num / 1e54 >=1 then
 		newnum = num / 1e54
-		return Library.Round(newnum, 2).. "SD"
+		return Library.Round(newnum, (commanums or 2)).. "SD"
 	elseif num / 1e51 >=1 then
 		newnum = num / 1e51
-		return Library.Round(newnum, 2).. "SeD"
+		return Library.Round(newnum, (commanums or 2)).. "SeD"
 	elseif num / 1e48 >=1 then
 		newnum = num / 1e48
-		return Library.Round(newnum, 2).. "QD"
+		return Library.Round(newnum, (commanums or 2)).. "QD"
 	elseif num / 1e45 >=1 then
 		newnum = num / 1e45
-		return Library.Round(newnum, 2).. "QD"
+		return Library.Round(newnum, (commanums or 2)).. "QD"
 	elseif num / 1e42 >=1 then
 		newnum = num / 1e42
-		return Library.Round(newnum, 2).. "TD"
+		return Library.Round(newnum, (commanums or 2)).. "TD"
 	elseif num / 1e39 >=1 then
 		newnum = num / 1e39
-		return Library.Round(newnum, 2).. "DD"
+		return Library.Round(newnum, (commanums or 2)).. "DD"
 	elseif num / 1e36 >=1 then
 		newnum = num / 1e36
-		return Library.Round(newnum, 2).. "UD"
+		return Library.Round(newnum, (commanums or 2)).. "UD"
 	elseif num / 1e33 >=1 then
 		newnum = num / 1e33
-		return Library.Round(newnum, 2).. "De"
+		return Library.Round(newnum, (commanums or 2)).. "De"
 	elseif num / 1e30 >=1 then
 		newnum = num / 1e30
-		return Library.Round(newnum, 2).. "No"
+		return Library.Round(newnum, (commanums or 2)).. "No"
 	elseif num / 1e27 >=1 then
 		newnum = num / 1e27
-		return Library.Round(newnum, 2).. "Oc"
+		return Library.Round(newnum, (commanums or 2)).. "Oc"
 	elseif num / 1e24 >=1 then
 		newnum = num / 1e24
-		return Library.Round(newnum, 2).. "Sp"
+		return Library.Round(newnum, (commanums or 2)).. "Sp"
 	elseif num / 1e21 >=1 then
 		newnum = num / 1e21
-		return Library.Round(newnum, 2).. "Se"
+		return Library.Round(newnum, (commanums or 2)).. "Se"
 	elseif num / 1e18 >=1 then
 		newnum = num / 1e18
-		return Library.Round(newnum, 2).. "Qn"
+		return Library.Round(newnum, (commanums or 2)).. "Qn"
 	elseif num / 1e15 >=1 then
 		newnum = num / 1e15
-		return Library.Round(newnum, 2).. "Qa"
+		return Library.Round(newnum, (commanums or 2)).. "Qa"
 	elseif num / 1e12 >=1 then
 		newnum = num / 1e12
-		return Library.Round(newnum, 2).. "T"
+		return Library.Round(newnum, (commanums or 2)).. "T"
 	elseif num / 1e09 >=1 then
 		newnum = num / 1e09
-		return Library.Round(newnum, 2).. "B"
+		return Library.Round(newnum, (commanums or 2)).. "B"
 	elseif num / 1e06 >=1 then
 		newnum = num / 1e06
-		return Library.Round(newnum, 2).. "M"
+		return Library.Round(newnum, (commanums or 2)).. "M"
 	elseif num / 1e03 >=1 then
 		newnum = num / 1e03
-		return Library.Round(newnum, 2).. "K"
+		return Library.Round(newnum, (commanums or 2)).. "K"
 	else return num
 	end
 end
-
 return Library
