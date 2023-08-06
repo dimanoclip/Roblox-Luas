8c9c655ab626dda27751a57ec35a82e19a94031f
Useless <91428127+Dimanoname@users.noreply.github.com>
2023-08-06 14:06:21 +0300

    1

---
+++ CheckingModuleScripts.lua
@@ -0,0 +1,13 @@
+--[[ 
+if you dont know a path for ur modulescript,
+you can use this script:
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
+]]--
+for i,v in require(ModuleScriptPathHere) do
+	local toprint = (i.." = {\n")
+	for i,v in pairs(v) do
+		toprint = (toprint.."				"..i.." = "..tostring(v).."\n")
+	end
+	toprint = (toprint.."			}")
+	print(toprint)
+end
