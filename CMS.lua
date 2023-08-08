16af807fc43bd12bab7e363890fa5b495315ecda
Useless <91428127+Dimanoname@users.noreply.github.com>
2023-08-08 14:00:11 +0300

    1

--- CMS.lua
+++ CMS.lua
@@ -4,14 +4,28 @@ you can use this script:
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
 ]]--
 local Library = {}
-Library.CMS = function(msp)
-	for i,v in require(ModuleScriptPathHere) do
-		local toprint = (i.." = {\n")
-		for i,v in pairs(v) do
-			toprint = (toprint.."				"..i.." = "..tostring(v).."\n")
-		end
-		toprint = (toprint.."			}")
-		print(toprint)
-	end
+Library.CMS = function(tbl, indent)
+  if not indent then indent = 0 end
+  local toprint = string.rep(" ", indent) .. "{\r\n"
+  indent = indent + 2
+  for k, v in pairs(tbl) do
+    toprint = toprint .. string.rep(" ", indent)
+    if (type(k) == "number") then
+      toprint = toprint .. "[" .. k .. "] = "
+    elseif (type(k) == "string") then
+      toprint = toprint  .. k ..  "= "
+    end
+    if (type(v) == "number") then
+      toprint = toprint .. v .. ",\r\n"
+    elseif (type(v) == "string") then
+      toprint = toprint .. "\"" .. v .. "\",\r\n"
+    elseif (type(v) == "table") then
+      toprint = toprint .. tprint(v, indent + 2) .. ",\r\n"
+    else
+      toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
+    end
+  end
+  toprint = toprint .. string.rep(" ", indent-2) .. "}"
+  return toprint
 end
 return Library
