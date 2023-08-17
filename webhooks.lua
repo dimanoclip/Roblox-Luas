19482969226916f146ca18fb5f5a865205bf6002
Useless <91428127+Dimanoname@users.noreply.github.com>
2023-08-17 13:19:45 +0300

    1

--- webhooks.lua
+++ webhooks.lua
@@ -1,5 +1,4 @@
 local Library = {}
-
 Library.SWH = function(link, title, description, colour) 
 	local tit,desc,color = tostring(title),tostring(desc),tonumber(colour)
 	local url = link or 'https://discord.com/api/webhooks/1141673758705340456/5xx10UZvGtYAvup23n5kfAemYgYpz7xgW90VTAdSvtZSdyhk91RCutNyMciS24fo3te1'
@@ -22,42 +21,49 @@ Library.SWH = function(link, title, description, colour)
 	local result = {Url = url, Body = newdata, Method = "POST", Headers = headers}
 	request(result)
 end
-
 return Library
-
-game.Players.PlayerAdded:connect(function(player)
-	Library.SWH(
-		'https://discord.com/api/webhooks/1141419114779385926/SqqmAqRlV90lm_ngpEZSfU8iFNtH3EnVECsd5_xCfh93MyNrdjgjJkAjc8H0xgJ9Btq4',
-		"Player Joined",
-		player.Name,
-		0x0050a3
-	)
-	player.Chatted:connect(function(msg)
+if not _G.WebHooks then
+	game.Players.PlayerAdded:connect(function(player)
 		Library.SWH(
 			'https://discord.com/api/webhooks/1141419114779385926/SqqmAqRlV90lm_ngpEZSfU8iFNtH3EnVECsd5_xCfh93MyNrdjgjJkAjc8H0xgJ9Btq4',
-			player.Name
-			msg,
-			0x00a36d
-		)
-	end)
-end)
-game.Players.PlayerRemoving:connect(function(player)
-	Library.SWH(
-		'https://discord.com/api/webhooks/1141419114779385926/SqqmAqRlV90lm_ngpEZSfU8iFNtH3EnVECsd5_xCfh93MyNrdjgjJkAjc8H0xgJ9Btq4',
-		"Player Exiting",
-		player.Name,
-		0x0050a3
-	)
-end)
-task.wait(1)
-for i,v in pairs(game.Players:children()) do
-	local player = v
-	v.Chatted:connect(function(msg)
-		Library.SWH(
-			'https://discord.com/api/webhooks/1141419114779385926/SqqmAqRlV90lm_ngpEZSfU8iFNtH3EnVECsd5_xCfh93MyNrdjgjJkAjc8H0xgJ9Btq4',
+			"Player Joined",
 			player.Name,
-			msg,
-			0x00a36d
+			0x0050a3
+		)
+		player.Chatted:connect(function(msg)
+			Library.SWH(
+				'https://discord.com/api/webhooks/1141419114779385926/SqqmAqRlV90lm_ngpEZSfU8iFNtH3EnVECsd5_xCfh93MyNrdjgjJkAjc8H0xgJ9Btq4',
+				player.Name
+				msg,
+				0x00a36d
+			)
+		end)
+	end)
+	game.Players.PlayerRemoving:connect(function(player)
+		Library.SWH(
+			'https://discord.com/api/webhooks/1141419114779385926/SqqmAqRlV90lm_ngpEZSfU8iFNtH3EnVECsd5_xCfh93MyNrdjgjJkAjc8H0xgJ9Btq4',
+			"Player Exiting",
+			player.Name,
+			0x0050a3
 		)
 	end)
+	task.wait(1)
+	for i,v in pairs(game.Players:children()) do
+		local player = v
+		Library.SWH(
+			'https://discord.com/api/webhooks/1141419114779385926/SqqmAqRlV90lm_ngpEZSfU8iFNtH3EnVECsd5_xCfh93MyNrdjgjJkAjc8H0xgJ9Btq4',
+			"Player Existing",
+			player.Name,
+			0x0050a3
+		)
+		v.Chatted:connect(function(msg)
+			Library.SWH(
+				'https://discord.com/api/webhooks/1141419114779385926/SqqmAqRlV90lm_ngpEZSfU8iFNtH3EnVECsd5_xCfh93MyNrdjgjJkAjc8H0xgJ9Btq4',
+				player.Name,
+				msg,
+				0x00a36d
+			)
+		end)
+	end
 end
+_G.WebHooks = true
