6267885fa255a28075a7c193cae5586430dc98de
Useless <91428127+Dimanoname@users.noreply.github.com>
2023-08-17 13:40:36 +0300

    1

--- webhooks.lua
+++ webhooks.lua
@@ -1,27 +1,3 @@
-local Library = {}
-Library.SWH = function(link, title, description, colour) 
-	local tit,desc,color = tostring(title),tostring(desc),tonumber(colour)
-	local url = link or 'https://discord.com/api/webhooks/1141673758705340456/5xx10UZvGtYAvup23n5kfAemYgYpz7xgW90VTAdSvtZSdyhk91RCutNyMciS24fo3te1'
-	local data = {
-		["content"] = "",
-		["embeds"] = {{
-			["title"] = tit,
-			["description"] = desc,
-			["type"] = "rich",
-			["color"] = color or tonumber(0x000000),
-			["footer"] = {
-				["text"] = "UselessBot",
-				["icon_url"] = "https://i.imgur.com/x4q1HDg.png"
-			},
-		}}
-	}
-	local newdata = game:GetService("HttpService"):JSONEncode(data)
-	local headers = {["content-type"] = "application/json"}
-	request = http_request or request or HttpPost or syn.request
-	local result = {Url = url, Body = newdata, Method = "POST", Headers = headers}
-	request(result)
-end
-return Library
 if not _G.WebHooks then
 	game.Players.PlayerAdded:connect(function(player)
 		Library.SWH(
