b6acb6576e5ee53fcd76043c71a44aaaa43a8429
Useless <91428127+Dimanoname@users.noreply.github.com>
2023-09-17 10:16:33 +0300

    1

--- wh.lua
+++ wh.lua
@@ -1,5 +1,5 @@
 local Library = {}
-Library.SWH = function(link, title, desc, colour) 
+Library.Send = function(link, title, desc, colour) 
 	local color = tonumber(colour)
 	local url = link or 'https://discord.com/api/webhooks/1141673758705340456/5xx10UZvGtYAvup23n5kfAemYgYpz7xgW90VTAdSvtZSdyhk91RCutNyMciS24fo3te1'
 	local data = {
