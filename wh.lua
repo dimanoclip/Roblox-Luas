628f8e5a68b1596aa28b627b86e007d8978bff10
Useless <91428127+Dimanoname@users.noreply.github.com>
2023-09-24 14:14:08 +0300

    1

--- wh.lua
+++ wh.lua
@@ -1,16 +1,15 @@
 local Library = {}
 local HttpService = game:GetService("HttpService")
 local function encode(table: table) return HttpService:JSONEncode(table) end
-Library.Send = function(link, title, desc, color) 
-	local color = tonumber(color)
-	local url = link or 'https://discord.com/api/webhooks/1141673758705340456/5xx10UZvGtYAvup23n5kfAemYgYpz7xgW90VTAdSvtZSdyhk91RCutNyMciS24fo3te1'
+Library.Send = function(link, title, desc, color)
+	color = tonumber(color)
+	link = link or 'https://discord.com/api/webhooks/1141673758705340456/5xx10UZvGtYAvup23n5kfAemYgYpz7xgW90VTAdSvtZSdyhk91RCutNyMciS24fo3te1'
 	local data = {
 		["content"] = "",
 		["embeds"] = {{
             ["author"] = {
-                ["name"] = "Useless webhook",
-                ["url"] = "https://github.com/Dimanoname/1/blob/main/wh.lua",
-                ["icon_url"] = "https://i.imgur.com/x4q1HDg.png"
+                ["name"] = game.PlaceId,
+                ["url"] = "https://github.com/Dimanoname/1/blob/main/wh.lua"
             },
 			["title"] = title or "Roblox",
             ["url"] = "https://www.roblox.com/games/"..game.PlaceId,
@@ -23,10 +22,16 @@ Library.Send = function(link, title, desc, color)
 			}
 		}}
 	}
-	local newdata = encode(data)
-	local headers = {["content-type"] = "application/json"}
+	data = encode(data)
 	local request = http_request or request or HttpPost or syn.request
-	local result = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+	local result = {
+		Url = link,
+		Body = data,
+		Method = "POST",
+		Headers = {
+			["content-type"] = "application/json"
+		}
+	}
 	request(result)
 end
 return Library
