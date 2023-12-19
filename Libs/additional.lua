e9ff96a09014d7964fde2c0865935a65075411f5
Dimanoname <gamefolover2019@gmail.com>
2023-12-19 21:01:09 +0200

    1

--- Libs/additional.lua
+++ Libs/additional.lua
@@ -1,3 +1,4 @@
+--https://raw.githubusercontent.com/Dimanoname/Roblox-Luas/main/Libs/additional.lua
 local Library = {}
 Library.Children = function(table, callback)
     if table and (type(table) == "userdata" or type(table) == "table") then local tablename,ttable
@@ -7,5 +8,9 @@ Library.Children = function(table, callback)
         warn(string.format("%s:",tablename))
         for i,v in table do local type = v.ClassName callback(i,v,type) end
     else return end
+end
+Library.dist = function(pos1,pos2)
+    if not pos2 then pos2 = (game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")).Position end
+    return math.floor((pos2 - pos1).magnitude)
 end
 return Library
