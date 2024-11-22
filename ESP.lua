3f7f7c33674f71802e40d6bf7fc51e75afd69503
Dimanoname <gamefolover2019@gmail.com>
2024-11-22 17:25:02 +0200

    1

--- ESP.lua
+++ ESP.lua
@@ -2,17 +2,16 @@ if not game:IsLoaded() then
 	game.Loaded:Wait()
 end
 --Press F5 To Toggle ESP--
-local msg = loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/1/main/messages.lua"))()
 local adds = loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/Roblox-Luas/main/Libs/additional.lua"))()
-msg.Notify("Binds", "F5 - ESP")
-local Toggle = true
-local PlayerName = "DisplayName"
 local P = game:GetService("Players")
 local LP = P.LocalPlayer
-local UserInputService=game:GetService("UserInputService")
 local RunService=game:GetService("RunService")
 local DB = false
-local ESPT = function(PLR)
+local ESP_players = {}
+local Library = {}
+Library.add_player = function(player) table.insert(ESP_players, player.Name, player) end
+Library.rem_player = function(player) table.remove(ESP_players, player.Name) end
+local CreateESP = function(PLR)
 	local Icon = Instance.new("BillboardGui")
 	local NameDist = Instance.new("TextLabel")
 	Icon.Parent = PLR.Character:FindFirstChild('Head')
@@ -31,11 +30,11 @@ local ESPT = function(PLR)
 	NameDist.Size = UDim2.new(0, 400, 0, 10)
 	NameDist.Font = Enum.Font.IndieFlower
 	NameDist.Text = "NIL"
-	NameDist.TextColor3 = PLR.TeamColor.Color
+	NameDist.TextColor3 = Color3.fromRGB(160, 0, 0)
 	NameDist.TextSize = 18.000
 	NameDist.TextWrapped = true
 end
-local ESPTF = function(PLR, ICON)
+local UpdateESP = function(PLR, ICON)
 	local NameDist = Instance.new("TextLabel")
 	NameDist.Name = "NameDist"
 	NameDist.Parent = ICON
@@ -45,44 +44,37 @@ local ESPTF = function(PLR, ICON)
 	NameDist.Size = UDim2.new(0, 400, 0, 10)
 	NameDist.Font = Enum.Font.IndieFlower
 	NameDist.Text = "NIL"
-	NameDist.TextColor3 = PLR.TeamColor.Color
+	NameDist.TextColor3 = Color3.fromRGB(160,0,0)
 	NameDist.TextSize = 18.000
 	NameDist.TextWrapped = true
 end
 RunService.Stepped:Connect(function()
-	if not Toggle then
-		for i,v in pairs(P:GetChildren()) do
-			if v.Character:FindFirstChild('Head'):FindFirstChild("Icon") ~= nil then
-				v.Character:FindFirstChild('Head'):FindFirstChild("Icon"):Remove()
-			end
+	for i,v in pairs(P:GetChildren()) do
+		if v.Character:FindFirstChild('Head'):FindFirstChild("Icon") ~= nil then
+			v.Character:FindFirstChild('Head'):FindFirstChild("Icon"):Remove()
 		end
 	end
 	if DB then return end
 	DB = true
 	pcall(function()
-		if Toggle then
-			for i,v in pairs(P:GetChildren()) do
-				if v:IsA("Player") then
-					if v ~= game.Players.LocalPlayer then
-						if v.Character ~= nil then
-							if v.Character:FindFirstChild('Head'):FindFirstChild("Icon") == nil then
-								ESPT(v)
+		if not ESP_players[1] then return end
+		for i,v in pairs(ESP_players) do
+			if v:IsA("Player") then
+				if v == game.Players.LocalPlayer or v.Character == nil then continue end
+				if v.Character:FindFirstChild('Head'):FindFirstChild("Icon") == nil then
+					CreateESP(v)
+				else
+					if v.Character:FindFirstChild('Head').Icon:FindFirstChild("NameDist") == nil then
+						UpdateESP(v, v.Character:FindFirstChild('Head').Icon)
+					else
+						if v.Character:FindFirstChild('Head'):FindFirstChild("Icon").Enabled == false then
+							v.Character:FindFirstChild('Head'):FindFirstChild("Icon").Enabled = true
+						else
+							if v.Character:FindFirstChild('Head'):FindFirstChild("Icon") and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") ~= nil then
+								v.Character:FindFirstChild('Head').Icon["NameDist"].Text = v.Name..'| '..adds.dist_to(v)
 							else
-								if v.Character:FindFirstChild('Head').Icon:FindFirstChild("NameDist") == nil then
-									ESPTF(v, v.Character:FindFirstChild('Head').Icon)
-								else
-									if v.Character:FindFirstChild('Head'):FindFirstChild("Icon").Enabled == false then
-										v.Character:FindFirstChild('Head'):FindFirstChild("Icon").Enabled = true
-									else
-										if v.Character:FindFirstChild('Head'):FindFirstChild("Icon") and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") ~= nil then
-											local pos = adds.dist((v.Character:FindFirstChild("HumanoidRootPart")).Position)
-											v.Character:FindFirstChild('Head').Icon["NameDist"].Text = v.DisplayName.." | "..pos
-										else
-											if v.Character:FindFirstChild('Head'):FindFirstChild("Icon") and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") == nil then
-												v.Character:FindFirstChild('Head').Icon["NameDist"].Text = v.DisplayName
-											end
-										end
-									end
+								if v.Character:FindFirstChild('Head'):FindFirstChild("Icon") and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") == nil then
+									v.Character:FindFirstChild('Head').Icon["NameDist"].Text = v.Name
 								end
 							end
 						end
@@ -91,12 +83,6 @@ RunService.Stepped:Connect(function()
 			end
 		end
 	end)
-	wait()
+	task.wait()
 	DB = false
-end)
------------------------------------------------------------------
-UserInputService.InputBegan:connect(function(a,b)
-   if a.KeyCode==Enum.KeyCode.F1 then
-       Toggle = not Toggle
-   end
 end)
