4a305382686c5a16def5b1194a6038ef2c5423dd
dimanoclip <gamefolover2019@gmail.com>
2025-12-01 19:26:28 +0300

    1

--- Libs/msgs.lua
+++ Libs/msgs.lua
@@ -1,5 +1,5 @@
 local Library = {}
-local colors = {
+Library.hex_colors = {
     red = '#BE0000',
     mint = '#00FFBE',
     green = '#00FF32',
@@ -12,13 +12,26 @@ local colors = {
     gray = '#3C3C3C',
     white = '#ffffff'
 }
+Library.rgb_colors = {
+    red = Color3.fromRGB(190,0,0),
+    mint = '#00FFBE',
+    green = Color3.fromRGB(0,255,190),
+    blue = Color3.fromRGB(0,120,255),
+    purple = Color3.fromRGB(200,0,255),
+    pink = '#C800FF',
+    yellow = Color3.fromRGB(255, 200,0),
+    orange = Color3.fromRGB(255,150,0),
+    black = Color3.fromRGB(0,0,0),
+    gray = Color3.fromRGB(60,60,60),
+    white = Color3.fromRGB(255,255,255)
+}
 local TextChatService = game:GetService("TextChatService")
 Library.Link = "https://raw.githubusercontent.com/Dimanoname/Roblox-Luas/main/Libs/msgs.lua"
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/Roblox-Luas/main/Libs/msgs.lua"))()
 Library.Chat = function(text:string, color:string)
-    if color and type(color) == "string" and colors[color:lower()] then
+    if color and type(color) == "string" and Library.hex_colors[color:lower()] then
         TextChatService.TextChannels.RBXSystem:DisplaySystemMessage(
-            string.format("<b> <font color='%s' face='Code' size='18'>%s</font> </b>", colors[color] and colors[color] or color, text)
+            string.format("<font color='%s' face='Code' size='18'>%s</font>", Library.hex_colors[color] and Library.hex_colors[color] or color, text)
         )
     end
 end
