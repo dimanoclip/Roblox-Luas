bb8329d2adfba0575d7578db94e4e3f7e013ba17
dimanoclip <gamefolover2019@gmail.com>
2025-12-01 19:09:58 +0300

    1

--- Libs/vinp.lua
+++ Libs/vinp.lua
@@ -4,17 +4,17 @@ local Library = {}
 Library.Link = "https://raw.githubusercontent.com/Dimanoname/Roblox-Luas/main/Libs/vinp.lua"
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/Dimanoname/Roblox-Luas/main/Libs/vinp.lua"))()
 Library.MouseClick = function(x, y, wtime)
-  VirtualInputManager:SendMouseButtonEvent(x, y, 0, true, nil, 0)
+    VirtualInputManager:SendMouseButtonEvent(x, y, 0, true, nil, 0)
     if wtime then task.wait(wtime) end
     VirtualInputManager:SendMouseButtonEvent(x, y, 0, false, nil, 0)
 end
 Library.MoveMouse = function(x, y)
     VirtualInputManager:SendMouseMoveEvent(x, y, nil)
 end
-Library.PressKey = function(kc, wtime)
-    VirtualInputManager:SendKeyEvent(true, kc, false, nil)
+Library.PressKey = function(keycode, wtime)
+    VirtualInputManager:SendKeyEvent(true, keycode, false, nil)
     if wtime then task.wait(wtime) end
-    VirtualInputManager:SendKeyEvent(false, kc, false, nil)
+    VirtualInputManager:SendKeyEvent(false, keycode, false, nil)
 end
 Library.CenterMouseClick = function()
     Library.MouseClick(Camera.ViewportSize.X/2, Camera.ViewportSize.Y/2)
