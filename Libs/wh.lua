0772f018041b5640d7c81c883512862554a6fdb9
dimanoclip <gamefolover2019@gmail.com>
2026-03-08 19:56:20 +0300

    1

--- Libs/wh.lua
+++ Libs/wh.lua
@@ -7,21 +7,24 @@ Library.Send = function(title, desc, color, link)
 	local data = {
 		username = game.Players.LocalPlayer.Name,
 		avatar_url = "https://i.imgur.com/x4q1HDg.png",
-		["content"] = "",
-		["embeds"] = {{
-            ["author"] = {
-                ["name"] = `[{game.Players.LocalPlayer.Name}](https://www.roblox.com/users/{game.Players.LocalPlayer.UserId})`,
+		content = "",
+		embeds = {{
+            author = {
+                name = `Noclipov Webhooks`,
+				url = Library.Link
             },
-			["title"] = `[{game.PlaceId}](https://www.roblox.com/games/{game.PlaceId})`,
-			["fields"] = {
-				{name = "Source", value = `[github]({Library.Link})`, inline=true}
+			title = title or "Title",
+			fields = {
+				{name = "Source", value = `[github]({Library.Link})`, inline=true},
+				{name = "Place", value = `[{game.PlaceId}](https://www.roblox.com/games/{game.PlaceId})`, inline=true},
+				{name = "Sender", value = `[{game.Players.LocalPlayer.Name}](https://www.roblox.com/users/{game.Players.LocalPlayer.UserId})`, inline=true},
 			},
-			["description"] = desc or "smth happend",
-			["type"] = "rich",
-			["color"] = color,
-			["footer"] = {
-				["text"] = game.Players.LocalPlayer.Name,
-				["icon_url"] = "https://i.imgur.com/x4q1HDg.png"
+			description = desc or "Description",
+			type = "rich",
+			color = color,
+			footer = {
+				text = "Thanks for using!",
+				icon_url = "https://i.imgur.com/x4q1HDg.png"
 			}
 		}}
 	}
