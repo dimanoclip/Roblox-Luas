8b23dfc9cf52677d3cb3ebb97975eb0f2558486c
dimanoclip <gamefolover2019@gmail.com>
2026-03-08 19:49:11 +0300

    1

--- Libs/wh.lua
+++ Libs/wh.lua
@@ -5,14 +5,17 @@ Library.Send = function(title, desc, color, link)
 	color = tonumber(color) or tonumber(0x825AFF)
 	link = link or 'https://discord.com/api/webhooks/1445372335363325972/ilGbDuX56r0XY4dpcR0WeT5_ZWJpZ4fLiUz1t198Vs_DLS2L1D_Lu3yMJtXpEEGyNX-n'
 	local data = {
+		username = game.Players.LocalPlayer.Name,
+		avatar_url = "https://i.imgur.com/x4q1HDg.png",
 		["content"] = "",
 		["embeds"] = {{
             ["author"] = {
-                ["name"] = game.PlaceId,
-                ["url"] = "https://www.roblox.com/games/"..game.PlaceId
+                ["name"] = `[{game.Players.LocalPlayer.Name}](https://www.roblox.com/users/{game.Players.LocalPlayer.UserId})`,
             },
-			["title"] = title or "Roblox",
-            ["url"] = Library.Link,
+			["title"] = `[{game.PlaceId}](https://www.roblox.com/games/{game.PlaceId})`,
+			["fields"] = {
+				{name = "Source", value = `[github]({Library.Link})`, inline=false}
+			},
 			["description"] = desc or "smth happend",
 			["type"] = "rich",
 			["color"] = color,
